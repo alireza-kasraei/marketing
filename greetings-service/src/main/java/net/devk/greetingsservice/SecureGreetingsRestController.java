aaa5b17c9f8d71b03fbc60d3d2686bfb0663bc4c
alireza-kasraei <kasraei.ar@gmail.com>
2019-01-04 14:51:04 +0330

    change greetings-service as resource-server to use symmetric jwts

--- greetings-service/src/main/java/net/devk/greetingsservice/SecureGreetingsRestController.java
+++ greetings-service/src/main/java/net/devk/greetingsservice/SecureGreetingsRestController.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import java.util.Collections;
 import java.util.Map;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(method = RequestMethod.GET, value = "/greet/{name}")
 public class SecureGreetingsRestController {
 
+	@PreAuthorize("#oauth2.hasScope('openid')")
 	@RequestMapping
 	Map<String, String> hi(@PathVariable String name, Principal p) {
 		return Collections.singletonMap("greeting", "Hello, " + name + " from " + p.getName() + "!");
