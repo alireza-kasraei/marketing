cc74acf9666f0e67d3f9a7840bb0f054e99c75ee
alireza-kasraei <kasraei.ar@gmail.com>
2019-01-05 00:39:44 +0330

    add marketing-ui application as sample project for testing authorization code grant type

---
+++ marketing-ui/src/main/java/net/devk/marketing/ui/UiSecurityConfig.java
@@ -0,0 +1,18 @@
+package net.devk.marketing.ui;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableOAuth2Sso
+@Configuration
+public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**").permitAll().anyRequest().authenticated().and().logout().logoutUrl("/logout")
+				.and().httpBasic().disable();
+	}
+
+}
