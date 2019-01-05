96de0f62381f488a2d8f71b062b333ffe2d3b222
alireza-kasraei <kasraei.ar@gmail.com>
2019-01-05 10:55:57 +0330

    add new config for distinguishing between piblic and protected APIs

--- greetings-service/src/main/java/net/devk/greetingsservice/OAuth2ResourceServerConfigJwt.java
+++ greetings-service/src/main/java/net/devk/greetingsservice/OAuth2ResourceServerConfigJwt.java
@@ -18,51 +18,48 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class OAuth2ResourceServerConfigJwt extends ResourceServerConfigurerAdapter {
 
-    @Autowired
-    private CustomAccessTokenConverter customAccessTokenConverter;
+	@Autowired
+	private CustomAccessTokenConverter customAccessTokenConverter;
 
-    @Override
-    public void configure(final HttpSecurity http) throws Exception {
-        // @formatter:off
-                http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                    .and()
-                    .authorizeRequests().anyRequest().permitAll();
-        // @formatter:on                
-    }
+	@Override
+	public void configure(final HttpSecurity http) throws Exception {
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/public/**").permitAll().antMatchers("/api/**").authenticated();
+	}
 
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
-    }
+	@Override
+	public void configure(final ResourceServerSecurityConfigurer config) {
+		config.tokenServices(tokenServices());
+	}
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setAccessTokenConverter(customAccessTokenConverter);
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setAccessTokenConverter(customAccessTokenConverter);
 
-        converter.setSigningKey("123");
-        // final Resource resource = new ClassPathResource("public.txt");
-        // String publicKey = null;
-        // try {
-        // publicKey = IOUtils.toString(resource.getInputStream());
-        // } catch (final IOException e) {
-        // throw new RuntimeException(e);
-        // }
-        // converter.setVerifierKey(publicKey);
-        return converter;
-    }
+		converter.setSigningKey("123");
+		// final Resource resource = new ClassPathResource("public.txt");
+		// String publicKey = null;
+		// try {
+		// publicKey = IOUtils.toString(resource.getInputStream());
+		// } catch (final IOException e) {
+		// throw new RuntimeException(e);
+		// }
+		// converter.setVerifierKey(publicKey);
+		return converter;
+	}
 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
 
 }
--- greetings-service/src/main/java/net/devk/greetingsservice/OAuthResourceConfiguration.java
+++ greetings-service/src/main/java/net/devk/greetingsservice/OAuthResourceConfiguration.java
@@ -1,12 +1,7 @@
 package net.devk.greetingsservice;
 
-import org.springframework.context.annotation.Configuration;
-//@formatter:off
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-
-@Configuration
-@EnableResourceServer
-@EnableOAuth2Client
+//@Configuration
+//@EnableResourceServer
+//@EnableOAuth2Client
 class OAuthResourceConfiguration {
 }
