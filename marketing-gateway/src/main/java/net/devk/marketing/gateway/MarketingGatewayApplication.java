2996fb2463f377316f2536998eee639a77e290ce
alireza-kasraei <kasraei.ar@gmail.com>
2019-01-07 09:30:59 +0330

    add new gateway module to the cloud native app

---
+++ marketing-gateway/src/main/java/net/devk/marketing/gateway/MarketingGatewayApplication.java
@@ -0,0 +1,24 @@
+package net.devk.marketing.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MarketingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/gs/**").filters(f -> f.stripPrefix(1)).uri("lb://greetings-service")).build();
+	}
+
+}
