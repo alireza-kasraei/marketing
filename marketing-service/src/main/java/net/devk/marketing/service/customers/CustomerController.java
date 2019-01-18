06d131dd84a3160c702d1b3880933d69550f3963
alireza-kasraei <kasraei.ar@gmail.com>
2019-01-18 16:02:17 +0330

    fix typos

--- marketing-service/src/main/java/net/devk/marketing/service/customers/CustomerController.java
+++ marketing-service/src/main/java/net/devk/marketing/service/customers/CustomerController.java
@@ -1,5 +1,15 @@
 package net.devk.marketing.service.customers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.devk.marketing.service.ControllersConfig;
+import net.devk.marketing.service.customers.dto.CreateNewCustomerResponseDTO;
+
+@RequestMapping(path=ControllersConfig.API_PREFIX)
 public class CustomerController {
+	
+	
+	public ResponseEntity<CreateNewCustomerResponseDTO> createNewCustomer(){}
 
 }
