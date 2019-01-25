2b416eb665a6e33c4392f6f0c0d1eb7eca3da14f
alireza-kasraei <kasraei.ar@gmail.com>
2019-01-25 05:53:20 +0330

    fix docker configs

--- authorization-service/src/main/resources/users.sql
+++ authorization-service/src/main/resources/users.sql
@@ -1,2 +1,2 @@
-INSERT INTO users(id, active, email, first_name, last_name, mobile_number, pass_word, roles, user_name)	VALUES (1, true, 'omid@gmail.com', 'omid', 'moradzade', '98912384679854', 'omid123', 'ADMIN,USER', 'omid');
-INSERT INTO users(id, active, email, first_name, last_name, mobile_number, pass_word, roles, user_name)	VALUES (2, true, 'hamid@gmail.com', 'hamid', 'ghadimi', '98912234923429', 'hamid123', 'USER', 'hamid');
+INSERT INTO users(id, active, email, first_name, last_name, mobile_number, pass_word, roles, user_name)	VALUES (1, 1, 'omid@gmail.com', 'omid', 'moradzade', '98912384679854', 'omid123', 'ADMIN,USER', 'omid');
+INSERT INTO users(id, active, email, first_name, last_name, mobile_number, pass_word, roles, user_name)	VALUES (2, 1, 'hamid@gmail.com', 'hamid', 'ghadimi', '98912234923429', 'hamid123', 'USER', 'hamid');
