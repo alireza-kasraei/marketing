cb82d8390ccd59159d1ab06acb4237aae9bb2a12
alireza-kasraei <kasraei.ar@gmail.com>
2019-01-26 00:16:28 +0330

    delete initialization script of postgres db

--- init-postgres.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER docker;
-ALTER USER docker WITH PASSWORD 'docker';
-CREATE DATABASE "authorization";
-GRANT ALL PRIVILEGES ON DATABASE "authorization" TO docker;
-
-CREATE DATABASE "sample_db";
-GRANT ALL PRIVILEGES ON DATABASE "sample_db" TO docker;
