eb98b95120c0915b87fa604effb86e35a9aae721
alireza kasraei <koorosh01@gmail.com>
2019-02-03 01:30:35 +0330

    add pre persist event for setting registerDate

--- marketing-service/src/main/java/net/devk/marketing/service/model/AbstractModel.java
+++ marketing-service/src/main/java/net/devk/marketing/service/model/AbstractModel.java
@@ -4,8 +4,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
 
 import lombok.Data;
+import net.devk.marketing.service.util.DateUtils;
 
 @Data
 @MappedSuperclass
@@ -13,5 +15,10 @@ public abstract class AbstractModel {
 
 	@Column(name = "REGISTER_DATE")
 	private Date registerDate;
+
+	@PrePersist
+	public void onPrePersist() {
+		registerDate = DateUtils.now();
+	}
 
 }
